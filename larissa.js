1d170660b668e3690f1c95a0cc05ee1154c95d9b
ylarissaaa <146839947+ylarissaaa@users.noreply.github.com>
2023-10-06 08:18:43 -0300

    Add files via upload

---
+++ larissa.js
@@ -0,0 +1,24 @@
+function insert(numum){
+
+    var numerodois = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numerodois + numum;
+}
+
+function clean () {
+
+    document.getElementById('resultado').innerHTML = "";
+}
+
+function back () {
+
+    var voltar = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = voltar.substring(0, voltar.length -1);
+}
+
+function calculator() {
+    var calcular = document.getElementById('resultado').innerHTML;
+
+    if(calcular) {
+        document.getElementById('resultado').innerHTML = eval(calcular)
+    }
+}
